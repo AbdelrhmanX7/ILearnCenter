93d16d04ad0c12727f11bf0d0ddc2278b6c1c0a8
AbdelrhmanX7 <hadh2819@gmail.com>
2023-08-17 12:27:01 +0300

    add toast

--- src/components/atoms/toast/Toast.tsx
+++ src/components/atoms/toast/Toast.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { toast, Toaster } from "react-hot-toast";
+import { Button } from "../button";
 
 export const Toast = () => {
   return (
     <div>
       <Toaster position="bottom-right" />
-      <button onClick={() => toast.success("success")}>success</button>
-      <button onClick={() => toast.success("error")}>error</button>
+      <Button onClick={() => toast.success("success")} label="Success" />
+      <Button onClick={() => toast.error("error")} label="error" danger />
     </div>
   );
 };
